d6dda2d3b377374f45cf473b7e003790a57e0132
Kritik <kritiksah0@gmail.com>
2024-02-11 22:56:26 +0530

    chat app backend - seprate

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { connectMongoDb } from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "http://localhost:5173/, http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
 
-app.listen(port, () => {
+server.listen(PORT, () => {
   connectMongoDb();
-  console.log(`Server running on port ${port}`);
+  console.log(`Server Running on port ${PORT}`);
 });
